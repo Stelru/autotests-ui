d6f258daed3df6fe86ed1406106d5cfff1d42632
Stelru <r.zakharov@etpgpb.ru>
2025-08-15 00:53:29 +0300

    Знакомство с PageComponent.Практика работы с Page Component

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -7,19 +7,13 @@ from pages.create_course_page import CreateCoursePage
 
 @pytest.mark.regression
 @pytest.mark.courses
-def test_empty_courses_list(chromium_page_with_state: Page):
-    chromium_page_with_state.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
-    courses_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
-    result_icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
-    result_title = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
-    result_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
-    expect(courses_title).to_be_visible()
-    expect(courses_title).to_have_text('Courses')
-    expect(result_icon).to_be_visible()
-    expect(result_title).to_be_visible()
-    expect(result_title).to_have_text('There is no results')
-    expect(result_text).to_be_visible()
-    expect(result_text).to_have_text('Results from the load test pipeline will be displayed here')
+def test_empty_courses_list(courses_list_page: CoursesListPage):
+    courses_list_page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+    courses_list_page.sidebar.check_visible()
+    courses_list_page.navbar.check_visible("username1")
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_empty_view()
 
 
 new_course = {"title":          "Playwright",
