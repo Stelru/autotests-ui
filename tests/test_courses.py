31fbea6b02f094b2e37cf2cca24bf1ecf9f43e68
Stelru <r.zakharov@etpgpb.ru>
2025-08-15 20:51:54 +0300

    Практикуемся в реализации PageComponent.Практика

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -10,8 +10,7 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
     courses_list_page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
     courses_list_page.sidebar.check_visible()
     courses_list_page.navbar.check_visible("username1")
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
+    courses_list_page.toolbar_view.check_visible()
     courses_list_page.check_visible_empty_view()
 
 
@@ -28,22 +27,21 @@ def test_create_course(courses_list_page: CoursesListPage, create_course_page: C
     create_course_page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
     create_course_page.check_visible_create_course_button()
     create_course_page.check_disabled_create_course_button()
-    create_course_page.check_visible_image_preview_empty_view()
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
     create_course_page.check_visible_create_course_form(title="", estimated_time="", description="", max_score="0", min_score="0")
     create_course_page.check_visible_exercises_title()
     create_course_page.check_visible_create_exercise_button()
     create_course_page.check_visible_exercises_empty_view()
-    create_course_page.upload_preview_image(file="./testdata/files/image.png")
-    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
+    create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
     create_course_page.fill_create_course_form(title=new_course["title"],
                                                estimated_time=new_course["estimated_time"],
                                                description=new_course["description"],
                                                max_score=new_course["max_score"],
                                                min_score=new_course["min_score"])
     create_course_page.click_create_course_button()
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
-    courses_list_page.check_visible_course_card(title=new_course["title"],
+    courses_list_page.toolbar_view.check_visible()
+    courses_list_page.course_view.check_visible(title=new_course["title"],
                                                 estimated_time=new_course["estimated_time"],
                                                 max_score=new_course["max_score"],
                                                 min_score=new_course["min_score"],
