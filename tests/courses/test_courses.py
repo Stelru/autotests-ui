d2b1ed76a416e0515896057304e4e5d8c11ac292
Stelru <r.zakharov@etpgpb.ru>
2025-08-18 16:13:01 +0300

    Настройки автотестов с Pydantic.Подготовка

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -37,7 +37,7 @@ class TestCourses:
     def test_empty_courses_list(self,courses_list_page: CoursesListPage):
         courses_list_page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
         courses_list_page.sidebar.check_visible()
-        courses_list_page.navbar.check_visible("username1")
+        courses_list_page.navbar.check_visible("username")
         courses_list_page.toolbar_view.check_visible()
         courses_list_page.check_visible_empty_view()
 
--- tests/dashboard/test_dashboard.py
+++ tests/dashboard/test_dashboard.py
@@ -24,7 +24,7 @@ class TestDashboard:
     def test_dashboard_displaying(self,dashboard_page_with_state: DashboardPage):
         dashboard_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
         dashboard_page_with_state.sidebar.check_visible()
-        dashboard_page_with_state.navbar.check_visible("username1")
+        dashboard_page_with_state.navbar.check_visible("username")
         dashboard_page_with_state.dashboard_toolbar.check_visible()
         dashboard_page_with_state.scores_chart_view.check_visible("Scores")
         dashboard_page_with_state.courses_chart_view.check_visible("Courses")
