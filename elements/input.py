ed3417e0b3fc963247f6878dd4cb02801035675d
Stelru <r.zakharov@etpgpb.ru>
2025-08-19 23:34:52 +0300

    Практикуемся в интеграции покрытия в UI тесты. Подготовка

--- elements/input.py
+++ elements/input.py
@@ -4,14 +4,21 @@ from playwright.sync_api import Locator, expect
 from elements.base_element import BaseElement
 from tools.logger import get_logger
 logger = get_logger("INPUT")
-
+from ui_coverage_tool import ActionType
 class Input(BaseElement):
     @property
     def type_of(self) -> str:
         return "input"
 
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:
-            return super().get_locator(nth, **kwargs).locator('input')
+        return super().get_locator(nth, **kwargs).locator('input')
+
+    def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
+        # Переопределяем метод формирования XPath-селектора:
+        #  - сначала получаем общий селектор блока
+        #  - затем уточняем путь до самого <input>, добавляя '//input'
+        # Это нужно, чтобы трекер точно знал, с каким элементом шло взаимодействие.
+        return f'{super().get_raw_locator(**kwargs)}//input'
 
     def fill(self, value: str, nth: int = 0, **kwargs):
         step = f'Fill {self.type_of} "{self.name}" to value "{value}"'
@@ -19,6 +26,7 @@ class Input(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             locator.fill(value)
+        self.track_coverage(ActionType.FILL, nth, **kwargs)
 
     def check_have_value(self, value: str, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" has a value "{value}"'
@@ -26,3 +34,4 @@ class Input(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_have_value(value)
+        self.track_coverage(ActionType.VALUE, nth, **kwargs)
