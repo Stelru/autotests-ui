6e19798562841696407dcfeb7256b633bc95ad91
Stelru <r.zakharov@etpgpb.ru>
2025-08-19 23:48:35 +0300

    Практикуемся в интеграции покрытия в UI тесты. Практическое задание: Работа с инструментом ui-coverage-tool

--- elements/button.py
+++ elements/button.py
@@ -3,7 +3,10 @@ from playwright.sync_api import expect
 
 from elements.base_element import BaseElement
 from tools.logger import get_logger
+from ui_coverage_tool import ActionType
+
 logger = get_logger("BUTTON")
+
 
 class Button(BaseElement):
     @property
@@ -16,6 +19,7 @@ class Button(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_be_enabled()
+        self.track_coverage(ActionType.ENABLED, nth, **kwargs)
 
     def check_disabled(self, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" is disabled'
@@ -23,3 +27,4 @@ class Button(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_be_disabled()
+        self.track_coverage(ActionType.DISABLED, nth, **kwargs)
--- elements/textarea.py
+++ elements/textarea.py
@@ -2,11 +2,19 @@ from playwright.sync_api import Locator, expect
 import allure
 from elements.base_element import BaseElement
 from tools.logger import get_logger
+from ui_coverage_tool import ActionType
 logger = get_logger("TEXTAREA")
 
 class Textarea(BaseElement):
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         return super().get_locator(nth, **kwargs).locator('textarea').first
+
+    def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
+        # Переопределяем метод формирования XPath-селектора:
+        #  - сначала получаем общий селектор блока
+        #  - затем уточняем путь до самого <input>, добавляя '//input'
+        # Это нужно, чтобы трекер точно знал, с каким элементом шло взаимодействие.
+        return f'{super().get_raw_locator(**kwargs)}//textarea[1]'
 
     def fill(self, value: str, nth: int = 0, **kwargs):
         step = f'Fill {self.type_of} "{self.name}" to value "{value}"'
@@ -14,6 +22,7 @@ class Textarea(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             locator.fill(value)
+        self.track_coverage(ActionType.FILL, nth, **kwargs)
 
     def check_value(self, value: str, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" has a value "{value}"'
@@ -21,3 +30,4 @@ class Textarea(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_have_value(value)
+        self.track_coverage(ActionType.VALUE, nth, **kwargs)
